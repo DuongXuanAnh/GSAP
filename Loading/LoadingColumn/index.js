84aebbe051d639b7ac9337e6e5307c46b57b111c
DuongXuanAnh <thanhniennghiem98@gmail.com>
2024-12-28 14:24:50 +0100

    Tween to Gsap

--- Loading/LoadingColumn/index.js
+++ Loading/LoadingColumn/index.js
@@ -1,46 +1,38 @@
-TweenMax.staggerFrom(
-  ".block",
-  0.8,
-  {
-    width: "0%",
-    ease: Power1.easeIn,
-    delay: 2,
-  },
-  0.04
-);
+gsap.from(".block", 0.8, {
+  width: "0%",
+  ease: Power1.easeIn,
+  delay: 2,
+  stagger: 0.04,
+});
 
-TweenMax.to(".loader", 1, {
+gsap.to(".loader", 1, {
   x: 2,
   opacity: 0,
   ease: Expo.easeInOut,
   delay: 1.5,
 });
 
-TweenMax.staggerFrom(
-  ".nav > a, .about p, sub-header > a",
-  2,
-  {
-    opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut,
-    delay: 3,
-  },
-  0.06
-);
+gsap.from(".nav > a, .about p, sub-header > a", 2, {
+  opacity: 0,
+  y: 30,
+  ease: Expo.easeInOut,
+  delay: 3,
+  stagger: 0.06,
+});
 
-TweenMax.to(".box", 0.2, {
+gsap.to(".box", 0.2, {
   opacity: 1,
   ease: Expo.easeInOut,
   delay: 3.8,
 });
 
-TweenMax.to(".img", 0.2, {
+gsap.to(".img", 0.2, {
   opacity: 1,
   ease: Expo.easeInOut,
   delay: 4,
 });
 
-TweenMax.to(".box", 2.4, {
+gsap.to(".box", 2.4, {
   y: "-100%",
   ease: Expo.easeInOut,
   delay: 4,
