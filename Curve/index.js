5112b6e7e3057e332f23ffb20b7c1e0b55f58efa
DuongXuanAnh <thanhniennghiem98@gmail.com>
2024-12-28 14:09:23 +0100

    Curve

---
+++ Curve/index.js
@@ -0,0 +1,17 @@
+// var finalPath = "M 200 0 L 200 500 Q 100 250 200 0";
+// var initialPath = "M 200 0 L 200 500 Q 200 250 200 0";
+
+// var string = document.querySelector("#page1");
+
+// gsap.to("svg path", {
+//   attr: { d: finalPath },
+//   duration: 1,
+//   ease: "power1.out",
+// });
+
+// gsap.to("svg path", {
+//   attr: { d: initialPath },
+//   duration: 0.5,
+//   ease: "power1.out",
+//   delay: 1,
+// });
